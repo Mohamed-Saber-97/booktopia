6a1f99d3099cfae4bc92f7a48879f8e7ea07dcbd
Almohandis <ur843d@gmail.com>
2024-08-24 18:53:38 +0300

    [Account.java] phone number regex added, nullable checks added

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -1,26 +1,33 @@
 package model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "account")
 @Data
 public class Account {
+    @Id
     @Column(name = "id")
     private Long id;
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
-    @Column(name = "birthday")
+    @Column(name = "birthday", nullable = false)
     @Temporal(TemporalType.DATE)
     private LocalDate birthday;
-    @Column(name = "password")
+    @Column(name = "password", nullable = false)
     private String password;
     @Column(name = "job")
     private String job;
-    @Column(name = "email")
+    @Email
+    @Column(name = "email", nullable = false)
     private String email;
-    @Column(name = "phone_number")
+    @Column(name = "phone_number", nullable = false)
+    @Pattern(regexp = "^01[0-25]\\d{8}$")
     private String phoneNumber;
     @Embedded
     private Address address;
