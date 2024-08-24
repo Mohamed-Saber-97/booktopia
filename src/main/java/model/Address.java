7d518c7094cf99e1161159b39d9d8fbeb7fc985b
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-08-24 21:58:21 +0300

    utilize @NotNull from Hibernate Validator

--- src/main/java/model/Address.java
+++ src/main/java/model/Address.java
@@ -3,17 +3,22 @@ package model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 @Embeddable
 public class Address {
-    @Column(name = "street",nullable = false)
+    @Column(name = "street")
+    @NotNull
     private String street;
-    @Column(name = "city",nullable = false)
+    @Column(name = "city")
+    @NotNull
     private String city;
-    @Column(name = "zipcode", nullable = false)
+    @Column(name = "zipcode")
+    @NotNull
     private String zipcode;
-    @Column(name = "country", nullable = false)
+    @Column(name = "country")
+    @NotNull
     private String country;
 }
