320cd509931e4014abf86e510223459d14911d74
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-08-25 19:50:37 +0300

    Add [Category.java] and [Product.java] entities
    Detailed description of the changes made:
    * Implemented Category.java fields.
    * Implemented Product.java fields.
    * Mapped Entities between two classes.

---
+++ src/main/java/model/OrderProduct.java
@@ -0,0 +1,34 @@
+package model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_product")
+@IdClass(OrderProductsId.class)
+@Getter
+@Setter
+public class OrderProduct {
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+
+    private Product product;
+    @Column(name = "quantity")
+    @Min(value = 0)
+    private Integer quantity;
+
+    @Column(name = "price")
+    @DecimalMin(value = "0.0", inclusive = false)
+    private BigDecimal price;
+}
---
+++ src/main/java/model/OrderProductsId.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderProductsId {
+    private Long orderId;
+    private Long productId;
+}
