de4ff56d2e3bc237021daf8bb26f701630610d86
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-08-27 22:40:47 +0300

    Add EntityManagerFactory to test entities

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,15 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.awt.print.Book;
+
 public class Application {
     public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("booktopia");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.getTransaction().commit();
 
     }
 }
