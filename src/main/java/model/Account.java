e4644e75ff9e44ef0213e7a539f1043b973b3332
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-08-31 15:18:52 +0300

    fix the Account String sizes instead of the default and add database constraints

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -2,9 +2,7 @@ package model;
 
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.io.Serializable;
@@ -18,26 +16,36 @@ import java.time.LocalDate;
 @ToString
 @EqualsAndHashCode(callSuper = false)
 public class Account implements Serializable {
-    @Column(name = "name")
+
+    @Column(name = "name", length = 100, nullable = false)
     @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name must be up to 100 characters")
     private String name;
-    @Column(name = "birthday")
-    @NotBlank(message = "Birthday is required")
+
+    @Column(name = "birthday", nullable = false)
+    @NotNull(message = "Birthday is required")
     @Temporal(TemporalType.DATE)
     private LocalDate birthday;
-    @Column(name = "password")
+
+    @Column(name = "password", length = 60, nullable = false)
     @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
-    @Column(name = "job")
+
+    @Column(name = "job", length = 100)
+    @Size(max = 100, message = "Job title must be up to 100 characters")
     private String job;
+
     @Column(name = "email", unique = true)
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     private String email;
-    @Column(name = "phone_number", unique = true)
+
+    @Column(name = "phone_number", unique = true, length = 15, nullable = false)
     @Pattern(regexp = "^01[0-25]\\d{8}$", message = "Invalid phone number format")
     @NotBlank(message = "Phone number is required")
     private String phoneNumber;
+
     @Embedded
     @Valid
     private Address address;
