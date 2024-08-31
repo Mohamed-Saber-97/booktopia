83ffa5ee6d7a4b8f2b43d536bf2eda179212c96c
Almohandis <ur843d@gmail.com>
2024-08-31 10:40:08 +0300

    [HomeController][footer][header] added

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -2,17 +2,20 @@ package controller;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
+@WebServlet(value = "")
 public class HomeController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("index.jsp").include(request, response);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        request.setAttribute("pageTitle", "Home");
+        dispatcher.forward(request, response);
     }
 
 }
