58ba70ae009983835aefd5579c6271267bde1aa8
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-08-31 11:44:44 +0300

    Add Bean validation for [Account.java] entity

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -1,37 +1,44 @@
 package model;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
+import lombok.*;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
 @Embeddable
-public class Account {
-    @Column(name = "name",nullable = false)
-    @NotNull
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class Account implements Serializable {
+    @Column(name = "name")
+    @NotBlank(message = "Name is required")
     private String name;
-    @Column(name = "birthday",nullable = false)
-    @NotNull
+    @Column(name = "birthday")
+    @NotBlank(message = "Birthday is required")
     @Temporal(TemporalType.DATE)
     private LocalDate birthday;
-    @Column(name = "password",nullable = false)
-    @NotNull
+    @Column(name = "password")
+    @NotBlank(message = "Password is required")
     private String password;
     @Column(name = "job")
     private String job;
-    @Column(name = "email", unique = true,nullable = false)
+    @Column(name = "email", unique = true)
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     private String email;
-    @Column(name = "phone_number",nullable = false,unique = true)
-    @Pattern(regexp = "^01[0-25]\\d{8}$")
-    @NotNull
+    @Column(name = "phone_number", unique = true)
+    @Pattern(regexp = "^01[0-25]\\d{8}$", message = "Invalid phone number format")
+    @NotBlank(message = "Phone number is required")
     private String phoneNumber;
     @Embedded
+    @Valid
     private Address address;
 }
