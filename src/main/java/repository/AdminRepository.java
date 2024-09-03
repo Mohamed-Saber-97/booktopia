37ec7274105907f2ec0f5a666c9bcb70a4578904
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-09-04 02:20:13 +0300

    Create [AdminRepository.java] and implement findByEmail() method

--- src/main/java/repository/AdminRepository.java
+++ src/main/java/repository/AdminRepository.java
@@ -1,2 +1,18 @@
-package repository;public class AdminRepository {
+package repository;
+
+import base.BaseRepository;
+import jakarta.persistence.TypedQuery;
+import model.Admin;
+
+public class AdminRepository extends BaseRepository<Admin, Long> {
+    public AdminRepository() {
+        super(Admin.class);
+    }
+
+    public Admin findByEmail(String email) {
+        String jpql = "SELECT admin FROM Admin admin WHERE admin.account.email = :email";
+        TypedQuery<Admin> query = entityManager.createQuery(jpql, Admin.class);
+        query.setParameter("email", email);
+        return query.getSingleResult();
+    }
 }
