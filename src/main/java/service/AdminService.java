dcf224c006c5850076bd9f33413dbbe6a36858d5
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-09-04 02:24:26 +0300

    Create [AdminService.java] and implement findByEmail() and update() methods

--- src/main/java/service/AdminService.java
+++ src/main/java/service/AdminService.java
@@ -1,5 +1,23 @@
 package service;
 
-public class AdminService {
+import model.Admin;
+import repository.AdminRepository;
 
+public class AdminService {
+    private final AdminRepository adminRepository;
+
+    public AdminService() {
+        this.adminRepository = new AdminRepository();
+    }
+
+    public Admin update(Admin admin) {
+        Admin savedAdmin = findByEmail(admin.getAccount().getEmail());
+        savedAdmin.getAccount().setAddress(admin.getAccount().getAddress());
+        savedAdmin.setAccount(admin.getAccount());
+        return adminRepository.update(savedAdmin);
+    }
+
+    Admin findByEmail(String email) {
+        return adminRepository.findByEmail(email);
+    }
 }
