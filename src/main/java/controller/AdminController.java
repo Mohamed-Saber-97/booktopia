1acd9aef3a3c565c5dca31242d9107ee57228785
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-09-04 02:30:43 +0300

    Create [AdminController.java] and implement update() methods

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -1,5 +1,16 @@
 package controller;
 
-public class AdminController {
+import model.Admin;
+import service.AdminService;
 
+public class AdminController {
+    private final AdminService adminService;
+
+    public AdminController() {
+        adminService = new AdminService();
+    }
+
+    public Admin update(Admin admin) {
+        return adminService.update(admin);
+    }
 }
