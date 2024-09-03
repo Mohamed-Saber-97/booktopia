328d4d37ccd013b48bd7376175cb99a943418eaf
Almohandis <ur843d@gmail.com>
2024-09-03 11:28:55 +0300

    [ViewProfileFilter] allow admin to view their profile too

--- src/main/java/filter/ViewProfileFilter.java
+++ src/main/java/filter/ViewProfileFilter.java
@@ -10,9 +10,11 @@ import java.io.IOException;
 @WebFilter(urlPatterns = "/view-my-profile")
 public class ViewProfileFilter implements Filter {
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        if (httpRequest.getSession().getAttribute("user") == null) {
+        if (httpRequest.getSession().getAttribute("user") == null
+                && httpRequest.getSession().getAttribute("admin") == null) {
             ((HttpServletResponse) response).sendRedirect(httpRequest.getContextPath() + "/");
         } else {
             chain.doFilter(request, response);
