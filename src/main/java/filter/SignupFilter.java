8285fda0516f4cf60185b2f579180f72ded55ae0
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-09-05 01:04:04 +0300

    Improve [SignupFilter.java] class and remove duplicate code from the Validation and Creating Buyer

--- src/main/java/filter/SignupFilter.java
+++ src/main/java/filter/SignupFilter.java
@@ -3,14 +3,11 @@ package filter;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
-import model.Account;
-import model.Address;
 import model.Buyer;
+import utils.RequestBuilderUtil;
 import utils.ValidatorUtil;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.Map;
 
 @WebFilter(urlPatterns = "/signup")
@@ -27,20 +24,7 @@ public class SignupFilter implements Filter {
                 ((HttpServletRequest) request).getSession().setAttribute("pageTitle", "Sign up");
                 dispatcher.forward(request, response);
             } else {
-                Buyer buyer = new Buyer();
-                buyer.setAccount(new Account());
-                buyer.getAccount().setAddress(new Address());
-                buyer.getAccount().setName(request.getParameter("name"));
-                buyer.getAccount().setBirthday(LocalDate.parse(request.getParameter("birthday")));
-                buyer.getAccount().setJob(request.getParameter("job"));
-                buyer.getAccount().setEmail(request.getParameter("email"));
-                buyer.getAccount().setPassword(request.getParameter("password"));
-                buyer.setCreditLimit(BigDecimal.valueOf(Double.parseDouble(request.getParameter("creditLimit"))));
-                buyer.getAccount().setPhoneNumber(request.getParameter("phoneNumber"));
-                buyer.getAccount().getAddress().setCountry(request.getParameter("country"));
-                buyer.getAccount().getAddress().setCity(request.getParameter("city"));
-                buyer.getAccount().getAddress().setStreet(request.getParameter("street"));
-                buyer.getAccount().getAddress().setZipcode(request.getParameter("zipcode"));
+                Buyer buyer = RequestBuilderUtil.buildBuyerFromRequest(httpRequest);
                 request.setAttribute("buyer", buyer);
                 chain.doFilter(request, response);
             }
