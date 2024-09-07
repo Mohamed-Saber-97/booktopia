5450d12b2ee6b97a96a7565f276cf5321a9027e3
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-09-07 03:47:16 +0300

    Add [RequestParameterUtil.java] and [RequestAttributeUtil.java] Interfaces for literal String Request parameters and attributes

---
+++ src/main/java/utils/RequestAttributeUtil.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class RequestAttributeUtil {
+    public static final String ERROR = "error";
+    public static final String PAGE_TITLE = "pageTitle";
+    public static final String BUYER = "buyer";
+    public static final String ADMIN = "admin";
+    public static final String USER = "user";
+    public static final String INTERESTS = "interests";
+    public static final String YES = "Y";
+    public static final String COUNTRIES = "countries";
+    public static final String PRODUCTS = "products";
+    public static final String CATEGORIES = "categories";
+    private RequestAttributeUtil() {
+    }
+}
