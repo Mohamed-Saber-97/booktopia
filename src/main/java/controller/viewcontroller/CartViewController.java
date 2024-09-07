2d19f90efa18698a6cd0eb6dad228b78050c9301
Almohandis <ur843d@gmail.com>
2024-09-07 20:31:02 +0300

    [CartViewController] added

--- src/main/java/controller/viewcontroller/CartViewController.java
+++ src/main/java/controller/viewcontroller/CartViewController.java
@@ -1,5 +1,6 @@
 package controller.viewcontroller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,10 +9,14 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+import static utils.RequestAttributeUtil.PAGE_TITLE;
+
 @WebServlet(value = "/cart")
 public class CartViewController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        RequestDispatcher dispatcher = request.getRequestDispatcher("cart.jsp");
+        request.getSession().setAttribute(PAGE_TITLE, "Cart");
+        dispatcher.forward(request, response);
     }
 }
