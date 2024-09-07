0e726a34007af347e49fe1a7d2155b48bde925fc
Omar Rashid <omar.rashid96@outlook.com>
2024-09-07 21:39:07 +0300

    adding personal files

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,12 +2,30 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import repository.BuyerRepository;
+import model.*;
+
+
 public class Application {
     public static void main(String[] args) {
+
+
         EntityManagerFactory entityManagerFactory  = Persistence.createEntityManagerFactory("booktopia");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.getTransaction().commit();
+
+        BuyerRepository buyerRepository = new BuyerRepository();
+
+        Buyer b = entityManager.find(Buyer.class, 2);
+        Buyer b2 = entityManager.find(Buyer.class, 4);
+        Product p = entityManager.find(Product.class, 1);
+        Product p2 = entityManager.find(Product.class, 28);
+        Product p3 = entityManager.find(Product.class, 51);
+
+
+        buyerRepository.addToWishlist(b, p);
+        buyerRepository.removeFromWishlist(b,p2);
+
+
 
     }
 }
