45147e2ea057198188bfc25c6ad15c878e112c41
Almohandis <ur843d@gmail.com>
2024-09-08 22:37:11 +0300

    remove non-available products from cart in [CartViewController][WishlistViewController][CartValidator]

--- src/main/java/controller/viewcontroller/CartViewController.java
+++ src/main/java/controller/viewcontroller/CartViewController.java
@@ -1,19 +1,27 @@
 package controller.viewcontroller;
 
-import java.io.IOException;
-
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import static utils.RequestAttributeUtil.PAGE_TITLE;
+import model.Buyer;
+import validator.CartValidator;
+
+import java.io.IOException;
+
+import static utils.RequestAttributeUtil.*;
 
 @WebServlet(value = "/cart")
 public class CartViewController extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Buyer buyer = (Buyer) request.getSession().getAttribute(USER);
+        if (!CartValidator.isValid(buyer.getCart(), buyer)) {
+            request.setAttribute(ERROR, CartValidator.ERROR_MESSAGE);
+        }
         RequestDispatcher dispatcher = request.getRequestDispatcher("cart.jsp");
         request.getSession().setAttribute(PAGE_TITLE, "Cart");
         dispatcher.forward(request, response);
--- src/main/java/validator/CartValidator.java
+++ src/main/java/validator/CartValidator.java
@@ -3,25 +3,32 @@ package validator;
 import java.util.List;
 import java.util.Map;
 
+import controller.BuyerController;
 import controller.ProductController;
 import model.Buyer;
 import model.Product;
 
 public class CartValidator {
+    public static final String ERROR_MESSAGE = "Some products are no longer available, please retry again";
 
     private CartValidator() {
     }
 
-    public static void validate(Map<Product, Integer> cart, Buyer buyer) {
-        List<Product> availableProducts = new ProductController().findAllAvailable();
+    public static boolean isValid(Map<Product, Integer> cart, Buyer buyer) {
+        boolean isValid = true;
+        ProductController productController = new ProductController();
+        BuyerController buyerController = new BuyerController();
+        List<Product> availableProducts = productController.findAllAvailable();
         for (Map.Entry entry : cart.entrySet()) {
             if (!availableProducts.contains((Product) entry.getKey())) {
+                isValid = false;
                 cart.remove((Product) entry.getKey());
                 buyer.removeFromCart((Product) entry.getKey());
             } else if ((Integer) entry.getValue() > availableProducts.get(availableProducts.indexOf((Product) entry.getKey())).getQuantity()) {
-                cart.remove((Product) entry.getKey());
-                buyer.removeFromCart((Product) entry.getKey());
+                isValid = false;
+                buyerController.setBuyerCartProductQuantity(buyer, (Product) entry.getKey(), 1);
             }
         }
+        return isValid;
     }
 }
