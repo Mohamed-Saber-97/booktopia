22547a671df5a35799b5e3da3214c1d0829993dd
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-09-11 02:43:12 +0300

    Utilize the function Built in the Buyer Service layer to validate and update the cart items

--- src/main/java/validator/CartValidator.java
+++ src/main/java/validator/CartValidator.java
@@ -1,13 +1,7 @@
 package validator;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-import controller.BuyerController;
-import controller.ProductController;
 import model.Buyer;
-import model.Product;
+import service.BuyerService;
 
 public class CartValidator {
     public static final String ERROR_MESSAGE = "Some products are no longer available, please retry again";
@@ -15,26 +9,7 @@ public class CartValidator {
     private CartValidator() {
     }
 
-    public static boolean isValid(Map<Product, Integer> cart, Buyer buyer) {
-        boolean isValid = true;
-        ProductController productController = new ProductController();
-        BuyerController buyerController = new BuyerController();
-        List<Product> availableProducts = productController.findAllAvailable();
-        Iterator<Map.Entry<Product, Integer>> iterator = cart.entrySet().iterator();
-
-        while (iterator.hasNext()) {
-            Map.Entry<Product, Integer> entry = iterator.next();
-            Product product = entry.getKey();
-
-            if (!availableProducts.contains(entry.getKey())) {
-                isValid = false;
-                buyer.removeCartItem(product);
-            } else if (entry.getValue() > availableProducts.get(availableProducts.indexOf(product)).getQuantity()) {
-                isValid = false;
-//                buyerController.setBuyerCartProductQuantity(buyer, product, 1);
-            }
-        }
-
-        return isValid;
+    public static boolean isValid(Buyer buyer) {
+        return new BuyerService().validateAndUpdateCart(buyer);
     }
 }
