c5e68c36255ad0d1ad5dbc15daa05945b3d24e8c
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-09-15 07:07:22 +0300

    fix some request names

--- src/main/java/utils/RequestParameterUtil.java
+++ src/main/java/utils/RequestParameterUtil.java
@@ -5,12 +5,12 @@ public class RequestParameterUtil {
     public static final String EMAIL = "email";
     public static final String PASSWORD = "password";
     public static final String CONFIRM_PASSWORD = "confirmPassword";
-    public static final String BIRTHDAY = "birthday";
+    public static final String BIRTHDAY = "dob";
     public static final String JOB = "job";
     public static final String PHONE_NUMBER = "phoneNumber";
     public static final String COUNTRY = "country";
     public static final String CITY = "city";
-    public static final String ZIPCODE = "zipcode";
+    public static final String ZIPCODE = "zipCode";
     public static final String STREET = "street";
     public static final String CREDIT_LIMIT = "creditLimit";
     public static final String MINIMUM_PRICE = "minPrice";
