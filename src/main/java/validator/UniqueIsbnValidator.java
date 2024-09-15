5c18cbfca6caa72cde6632ff43f402bdcede1f4b
khaled <topmax42@gmail.com>
2024-09-15 22:12:10 +0300

    isbn validation servlet modified

--- src/main/java/validator/UniqueIsbnValidator.java
+++ src/main/java/validator/UniqueIsbnValidator.java
@@ -1,17 +1,29 @@
 package validator;
 
 import controller.ProductController;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.util.Arrays;
 
-public class UniqueIsbnValidator {
+@WebServlet("/ISBN-Validator")
+public class UniqueIsbnValidator extends HttpServlet{
     public static final String ERROR_MESSAGE = "ISBN already exists";
     public static final ProductController productController = new ProductController();
 
-    private UniqueIsbnValidator() {
-    }
-
     public static boolean isValid(String... isbns) {
         return Arrays.stream(isbns).noneMatch(productController::existsByIsbn);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _ISBN = request.getParameter("isbn");
+        String responseText = isValid(_ISBN) ? "true" : ERROR_MESSAGE;
+        response.setContentType("text/plain");
+        response.getWriter().write(responseText);
     }
 }
