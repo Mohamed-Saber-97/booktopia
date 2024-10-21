e666c47ce3e90c39507c60b046191475ed826b2a
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-21 23:32:03 +0300

    first commit

---
+++ src/test/java/org/example/booktopia/BooktopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.booktopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
