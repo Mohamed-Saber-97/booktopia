efbf3d26796d516e5e636d3f8ce71a39a77d1015
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-23 00:07:05 +0300

    implement two constructor annotation

--- src/main/java/org/example/booktopia/model/BuyerWishlistId.java
+++ src/main/java/org/example/booktopia/model/BuyerWishlistId.java
@@ -3,16 +3,15 @@ package org.example.booktopia.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotNull;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @Embeddable
 @EqualsAndHashCode
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class BuyerWishlistId {
     @NotNull
     @Column(name = "buyer_id", nullable = false)
