8d6cde9b64efe54fe2b0ad11a538aeff91dacdf1
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-23 00:14:48 +0300

    add BuyerWishlistRepository.java

---
+++ src/main/java/org/example/booktopia/repository/BuyerWishlistRepository.java
@@ -0,0 +1,17 @@
+package org.example.booktopia.repository;
+
+import jakarta.transaction.Transactional;
+import org.example.booktopia.model.BuyerWishlist;
+import org.example.booktopia.model.BuyerWishlistId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface BuyerWishlistRepository extends JpaRepository<BuyerWishlist, BuyerWishlistId> {
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM BuyerWishlist bw WHERE bw.id.buyerId = :buyerId AND bw.id.productId = :productId")
+    void removeProductFromWishlist(Long buyerId, Long productId);
+
+    boolean existsByBuyerIdAndProductId(Long buyerId, Long productId);
+}
