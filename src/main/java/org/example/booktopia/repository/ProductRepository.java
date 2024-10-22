51f84379cb710b5833e1d34cce39d37d8a4cef13
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-22 10:20:08 +0300

    Implement ProductRepository.java interface

---
+++ src/main/java/org/example/booktopia/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package org.example.booktopia.repository;
+
+import org.example.booktopia.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p FROM Product p where p.category.id IN :categoryIds")
+    List<Product> findAllByCategoryIds(@Param("categoryIds") List<Long> categoryIds);
+}
