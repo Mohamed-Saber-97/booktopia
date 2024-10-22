73962c2836fa3be9cd5d2e5be35a7c29649d5094
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-23 02:49:16 +0300

    add findById() method

--- src/main/java/org/example/booktopia/service/OrderService.java
+++ src/main/java/org/example/booktopia/service/OrderService.java
@@ -2,6 +2,7 @@ package org.example.booktopia.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.booktopia.error.RecordNotFoundException;
 import org.example.booktopia.model.Order;
 import org.example.booktopia.repository.OrderRepository;
 import org.springframework.data.domain.Page;
@@ -17,5 +18,10 @@ public class OrderService {
     public Page<Order> findAllByCustomerId(Long buyerId, int pageNumber, int pageSize) {
         PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
         return orderRepository.findAllByBuyerIdAndIsDeletedIsFalse(buyerId, pageRequest);
+    }
+
+    public Order findById(Long id) {
+        return orderRepository.findById(id)
+                              .orElseThrow(() -> new RecordNotFoundException("Order", "ID", id.toString()));
     }
 }
