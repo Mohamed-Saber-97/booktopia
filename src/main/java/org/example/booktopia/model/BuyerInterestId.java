afe5d23e8b7a3dbc58a4600f9df07f5259765cb8
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-23 00:06:03 +0300

    add EqualsAndHashCode and ToString annotations

--- src/main/java/org/example/booktopia/model/BuyerInterestId.java
+++ src/main/java/org/example/booktopia/model/BuyerInterestId.java
@@ -3,17 +3,17 @@ package org.example.booktopia.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.Hibernate;
-
-import java.util.Objects;
+import lombok.ToString;
 
 @Getter
 @Setter
 @Embeddable
-public class BuyerInterestId implements java.io.Serializable {
-    private static final long serialVersionUID = -6297751233471508002L;
+@EqualsAndHashCode
+@ToString
+public class BuyerInterestId {
     @NotNull
     @Column(name = "buyer_id", nullable = false)
     private Long buyerId;
@@ -21,19 +21,4 @@ public class BuyerInterestId implements java.io.Serializable {
     @NotNull
     @Column(name = "category_id", nullable = false)
     private Long categoryId;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        BuyerInterestId entity = (BuyerInterestId) o;
-        return Objects.equals(this.buyerId, entity.buyerId) &&
-                Objects.equals(this.categoryId, entity.categoryId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(buyerId, categoryId);
-    }
-
 }
