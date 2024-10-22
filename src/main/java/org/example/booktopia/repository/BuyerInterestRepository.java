c9ad28d8f8c1ebc9fd9b5466c5943a2c3b27dd8f
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-22 10:19:51 +0300

    Implement BuyerInterestRepository.java interface

---
+++ src/main/java/org/example/booktopia/repository/BuyerInterestRepository.java
@@ -0,0 +1,13 @@
+package org.example.booktopia.repository;
+
+import org.example.booktopia.model.BuyerInterest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface BuyerInterestRepository extends JpaRepository<BuyerInterest, Long> {
+    @Query("SELECT bi.id.categoryId FROM BuyerInterest bi where bi.id.buyerId = :buyerId")
+    List<Long> findCategoryIdsByBuyerId(@Param("buyerId") Long buyerId);
+}
