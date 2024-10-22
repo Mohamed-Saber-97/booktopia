494ac74418177ee4f89537328b63e8cc7ad70647
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-22 10:12:34 +0300

    Add CustomErrorAttributes Exception

---
+++ src/main/java/org/example/booktopia/error/CustomErrorAttributes.java
@@ -0,0 +1,26 @@
+package org.example.booktopia.error;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Component
+public class CustomErrorAttributes extends DefaultErrorAttributes {
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
+        errorAttributes.put("locale", webRequest.getLocale()
+                                                .toString());
+        errorAttributes.put("success", Boolean.FALSE);
+        errorAttributes.put("status", errorAttributes.get("error"));
+        errorAttributes.put("exception", errorAttributes.get("message"));
+        errorAttributes.put("details", Collections.singletonList(errorAttributes.get("message")));
+        errorAttributes.remove("error");
+        errorAttributes.remove("path");
+        return errorAttributes;
+    }
+}
