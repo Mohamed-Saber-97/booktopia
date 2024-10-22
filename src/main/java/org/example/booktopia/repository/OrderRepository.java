0fe9b7028eeef4d71d691a445e847930ff8eec11
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-23 00:14:56 +0300

    add OrderRepository.java

---
+++ src/main/java/org/example/booktopia/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package org.example.booktopia.repository;
+
+import org.example.booktopia.model.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findAllByBuyerIdAndIsDeletedIsFalse(Long customerId, Pageable pageable);
+}
