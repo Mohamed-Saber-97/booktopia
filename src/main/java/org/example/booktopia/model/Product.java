98c6a82bec310bf4c7c24df6155a48c2233758d5
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-22 11:06:34 +0300

    Extend BaseEntity to add fields

--- src/main/java/org/example/booktopia/model/Product.java
+++ src/main/java/org/example/booktopia/model/Product.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.example.booktopia.base.BaseEntity;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.math.BigDecimal;
@@ -16,7 +17,7 @@ import java.util.Set;
 @Setter
 @Entity
 @Table(name = "product", schema = "booktopia")
-public class Product {
+public class Product extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
