a3ef9b5483d818ca3848d89455f32dfb9bc161ce
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-23 00:16:06 +0300

    add OrderService.java

---
+++ src/main/java/org/example/booktopia/service/OrderService.java
@@ -0,0 +1,21 @@
+package org.example.booktopia.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.booktopia.model.Order;
+import org.example.booktopia.repository.OrderRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public Page<Order> findAllByCustomerId(Long buyerId, int pageNumber, int pageSize) {
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
+        return orderRepository.findAllByBuyerIdAndIsDeletedIsFalse(buyerId, pageRequest);
+    }
+}
