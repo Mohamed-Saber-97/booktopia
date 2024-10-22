71b832890098b348d320ccdcba4de0606e33c872
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-23 00:19:36 +0300

    implement function to clearCartByBuyerId()

--- src/main/java/org/example/booktopia/repository/CartItemRepository.java
+++ src/main/java/org/example/booktopia/repository/CartItemRepository.java
@@ -1,8 +1,15 @@
 package org.example.booktopia.repository;
 
+import jakarta.transaction.Transactional;
 import org.example.booktopia.model.CartItem;
 import org.example.booktopia.model.CartItemId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartItemRepository extends JpaRepository<CartItem, CartItemId> {
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM CartItem ci WHERE ci.buyer.id = :buyerId")
+    void clearCartByBuyerId(Long buyerId);
 }
