e7b53d0b28370ff0cbcef73e8b8337b0e8f97f09
Almohandis <67489100+Almohandis@users.noreply.github.com>
2024-10-24 08:19:20 +0300

    [Result] error helper method added

--- src/main/java/org/example/booktopia/utils/Result.java
+++ src/main/java/org/example/booktopia/utils/Result.java
@@ -1,13 +1,18 @@
 package org.example.booktopia.utils;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 @Data
-public class Result {
+@AllArgsConstructor
+public class Result<T> {
     private Boolean success;
     private HttpStatus status;
     private String message;
-    private Object data;
+    private T data;
 
+    public static <T> Result<T> error(HttpStatus status, String message) {
+        return new Result<>(false, status, message, null);
+    }
 }
