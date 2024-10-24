63b428d594a2580a0ed07bf65012b5ffcc05bb4a
Almohandis <67489100+Almohandis@users.noreply.github.com>
2024-10-24 22:50:58 +0300

    remove from buyer cart added

--- src/main/java/org/example/booktopia/controller/CartItemController.java
+++ src/main/java/org/example/booktopia/controller/CartItemController.java
@@ -2,6 +2,7 @@ package org.example.booktopia.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.booktopia.service.CartItemService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,12 @@ public class CartItemController {
     @PostMapping("/add/{buyerId}/{productId}/{quantity}")
     public ResponseEntity<?> addCartItem(@PathVariable("buyerId") Long buyerId, @PathVariable("productId") Long productId, @PathVariable("quantity") Integer quantity) {
         cartItemService.addCartItem(buyerId, productId, quantity);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/remove/{buyerId}/{productId}")
+    public ResponseEntity<?> removeCartItem(@PathVariable("buyerId") Long buyerId, @PathVariable("productId") Long productId) {
+        cartItemService.removeCartItem(buyerId, productId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
