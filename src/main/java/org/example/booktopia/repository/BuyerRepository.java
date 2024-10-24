993b80760aa7f89097b35d5d248f2dce23f0a2e0
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-24 07:18:10 +0300

    implement buyer login functionality

--- src/main/java/org/example/booktopia/repository/BuyerRepository.java
+++ src/main/java/org/example/booktopia/repository/BuyerRepository.java
@@ -4,6 +4,7 @@ import org.example.booktopia.model.Buyer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 
@@ -17,4 +18,7 @@ public interface BuyerRepository extends JpaRepository<Buyer, Long> {
     Optional<Buyer> findByAccountEmail(String accountEmail);
 
     Page<Buyer> findAllByIsDeletedIsFalse(Pageable pageable);
+
+    Optional<Buyer> findByAccount_EmailAndAccount_PasswordAndIsDeletedFalse(@NonNull String email,
+                                                                            @NonNull String password);
 }
--- src/main/java/org/example/booktopia/service/BuyerService.java
+++ src/main/java/org/example/booktopia/service/BuyerService.java
@@ -1,8 +1,13 @@
 package org.example.booktopia.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.booktopia.dtos.LoginDto;
+import org.example.booktopia.error.InvalidLoginCredentialsException;
 import org.example.booktopia.error.RecordNotFoundException;
+import org.example.booktopia.model.Account;
+import org.example.booktopia.model.Address;
 import org.example.booktopia.model.Buyer;
 import org.example.booktopia.repository.BuyerRepository;
 import org.springframework.data.domain.Page;
@@ -23,5 +28,24 @@ public class BuyerService {
     public Page<Buyer> findAllByPage(int pageNumber, int pageSize) {
         PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
         return buyerRepository.findAllByIsDeletedIsFalse(pageRequest);
+    }
+
+    public boolean login(LoginDto loginDto) {
+        Buyer buyer = buyerRepository.findByAccount_EmailAndAccount_PasswordAndIsDeletedFalse(loginDto.email(),
+                                                                                              loginDto.password())
+                                     .orElseThrow(InvalidLoginCredentialsException::new);
+
+//        if (!passwordEncoder.matches(password, admin.getAccount().getPassword())) {
+//            throw new InvalidLoginCredentialsException();
+//        }
+        return true;
+    }
+
+    public boolean existsByAccountEmail(String accountEmail) {
+        return buyerRepository.existsByAccountEmail(accountEmail);
+    }
+
+    public boolean existsByAccountPhone(String accountPhone) {
+        return buyerRepository.existsByAccountPhoneNumber(accountPhone);
     }
 }
