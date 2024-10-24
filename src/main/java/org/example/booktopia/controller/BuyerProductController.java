0e8647217c03713b6b27c5216ac08d98c1a3b755
Almohandis <67489100+Almohandis@users.noreply.github.com>
2024-10-24 21:52:19 +0300

    buyer product controller added

--- src/main/java/org/example/booktopia/controller/BuyerProductController.java
+++ src/main/java/org/example/booktopia/controller/BuyerProductController.java
@@ -5,9 +5,9 @@ import org.example.booktopia.dtos.ProductDto;
 import org.example.booktopia.service.BuyerProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
 
@@ -18,10 +18,9 @@ public class BuyerProductController {
 
     private final BuyerProductService buyerProductService;
 
-    @GetMapping
-    public ResponseEntity<List<ProductDto>> getBuyerInterestedProducts(Long buyerId) {
+    @GetMapping("{buyerId}")
+    public ResponseEntity<List<ProductDto>> getBuyerInterestedProducts(@PathVariable("buyerId") Long buyerId) {
         List<ProductDto> products = buyerProductService.getBuyerInterestedProducts(buyerId);
         return ResponseEntity.ok(products);
     }
-
 }
