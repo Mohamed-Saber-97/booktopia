9866909e7f555079efc05409f22e4797ff88954b
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-27 01:55:28 +0300

    implement Admin and Buyer service

--- src/main/java/org/example/booktopia/service/AdminService.java
+++ src/main/java/org/example/booktopia/service/AdminService.java
@@ -1,27 +1,83 @@
 package org.example.booktopia.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.booktopia.dtos.LoginDto;
-import org.example.booktopia.error.InvalidLoginCredentialsException;
+import org.example.booktopia.dtos.AdminDto;
+import org.example.booktopia.error.RecordNotFoundException;
+import org.example.booktopia.mapper.AdminMapper;
+import org.example.booktopia.model.Account;
+import org.example.booktopia.model.Address;
 import org.example.booktopia.model.Admin;
 import org.example.booktopia.repository.AdminRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class AdminService {
+public class AdminService implements UserDetailsService {
     private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AdminMapper adminMapper;
 
-    public boolean login(LoginDto loginDto) {
-        Admin admin = adminRepository.findByAccount_EmailAndAccount_PasswordAndIsDeletedFalse(loginDto.email(),
-                                                                                              loginDto.password())
-                                     .orElseThrow(InvalidLoginCredentialsException::new);
+    public AdminDto findByEmail(String email) {
+        var admin = adminRepository.findByAccountEmailAndIsDeletedIsFalse(email)
+                                   .orElseThrow(() -> new RecordNotFoundException("Admin", "Email", email));
+        return adminMapper.toDto(admin);
+    }
 
-//        if (!passwordEncoder.matches(password, admin.getAccount().getPassword())) {
-//            throw new InvalidLoginCredentialsException();
-//        }
-        return true;
+    public Admin findById(Long id) {
+        return adminRepository.findById(id)
+                              .orElseThrow(() -> new RecordNotFoundException("Admin", "Id", id.toString()));
+    }
+
+    public Admin findByPhonenumber(String phoneNumber) {
+        return adminRepository.findByAccountPhoneNumberAndIsDeletedIsFalse(phoneNumber)
+                              .orElseThrow(() -> new RecordNotFoundException("Admin", "Phonenumber", phoneNumber));
+    }
+
+    @Transactional
+    public AdminDto updateProfile(Long id, AdminDto adminDto) {
+        Admin currentAdmin = adminRepository.findById(id)
+                                            .orElseThrow(() -> new RecordNotFoundException("Admin", "Id", id
+                                                    .toString()));
+        Address address = Address.builder()
+                                 .country(adminDto.country())
+                                 .zipcode(adminDto.zipCode())
+                                 .street(adminDto.street())
+                                 .city(adminDto.city())
+                                 .build();
+        String encodedPassword = passwordEncoder.encode(adminDto.password());
+        Account account = Account.builder()
+                                 .name(adminDto.name())
+                                 .address(address)
+                                 .password(encodedPassword)
+                                 .birthday(adminDto.dob())
+                                 .job(adminDto.job())
+                                 .phoneNumber(adminDto.phoneNumber())
+                                 .email(adminDto.email())
+                                 .build();
+        currentAdmin.setAccount(account);
+        currentAdmin = adminRepository.save(currentAdmin);
+        return adminMapper.toDto(currentAdmin);
+    }
+
+    @Override
+//    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Admin admin = adminRepository.findByAccountEmailAndIsDeletedIsFalse(username)
+                                     .orElseThrow(() -> new UsernameNotFoundException(username));
+//        admin.getAccount()
+//             .setPassword(passwordEncoder.encode(username));
+        return User.builder()
+                   .username(admin.getUsername())
+                   .password(admin.getPassword())
+                   .roles("ADMIN")
+                   .build();
     }
 }
--- src/main/java/org/example/booktopia/service/BuyerService.java
+++ src/main/java/org/example/booktopia/service/BuyerService.java
@@ -15,37 +15,43 @@ import org.example.booktopia.model.Buyer;
 import org.example.booktopia.repository.BuyerRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class BuyerService {
+public class BuyerService implements UserDetailsService {
     private final BuyerRepository buyerRepository;
     private final BuyerMapper buyerMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public Buyer findById(Long id) {
         return buyerRepository.findById(id)
-                .orElseThrow(() -> new RecordNotFoundException("Buyer", "ID", id.toString()));
+                              .orElseThrow(() -> new RecordNotFoundException("Buyer", "ID", id.toString()));
     }
 
     @Transactional
     public void save(SignupDto signupDto) {
         Address address = Address.builder()
-                .street(signupDto.street())
-                .city(signupDto.city())
-                .zipcode(signupDto.zipCode())
-                .country(signupDto.country())
-                .build();
+                                 .street(signupDto.street())
+                                 .city(signupDto.city())
+                                 .zipcode(signupDto.zipCode())
+                                 .country(signupDto.country())
+                                 .build();
         Account account = Account.builder()
-                .name(signupDto.name())
-                .birthday(signupDto.dob())
-                .password(signupDto.password())
-                .job(signupDto.job())
-                .email(signupDto.email())
-                .phoneNumber(signupDto.phoneNumber())
-                .address(address)
-                .build();
+                                 .name(signupDto.name())
+                                 .birthday(signupDto.dob())
+                                 .password(signupDto.password())
+                                 .job(signupDto.job())
+                                 .email(signupDto.email())
+                                 .phoneNumber(signupDto.phoneNumber())
+                                 .address(address)
+                                 .build();
         Buyer buyer = new Buyer(account, signupDto.creditLimit(), signupDto.categories());
         buyerRepository.save(buyer);
     }
@@ -56,8 +62,9 @@ public class BuyerService {
     }
 
     public boolean login(LoginDto loginDto) {
-        Buyer buyer = buyerRepository.findByAccount_EmailAndAccount_PasswordAndIsDeletedFalse(loginDto.email(), loginDto.password())
-                .orElseThrow(InvalidLoginCredentialsException::new);
+        Buyer buyer = buyerRepository.findByAccount_EmailAndAccount_PasswordAndIsDeletedFalse(loginDto.email(),
+                                                                                              loginDto.password())
+                                     .orElseThrow(InvalidLoginCredentialsException::new);
 
 //        if (!passwordEncoder.matches(password, admin.getAccount().getPassword())) {
 //            throw new InvalidLoginCredentialsException();
@@ -75,7 +82,58 @@ public class BuyerService {
 
     public BuyerDto findByEmail(String email) {
         Buyer buyer = buyerRepository.findByAccountEmail(email)
-                .orElseThrow(() -> new RecordNotFoundException("Buyer", "Email", email));
+                                     .orElseThrow(() -> new RecordNotFoundException("Buyer", "Email", email));
         return buyerMapper.toDto(buyer);
     }
+
+    public Buyer findBuyerByEmail(String email) {
+        return buyerRepository.findByAccountEmail(email)
+                              .orElseThrow(() -> new RecordNotFoundException("Buyer", "Email", email));
+    }
+
+    public Buyer findBuyerByPhonenumber(String phonenumber) {
+        return buyerRepository.findByAccountPhoneNumber(phonenumber)
+                              .orElseThrow(() -> new RecordNotFoundException("Buyer", "Phonenumber", phonenumber));
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Buyer buyer = buyerRepository.findByAccountEmailAndIsDeletedIsFalse(username)
+                                     .orElseThrow(() -> new UsernameNotFoundException(username));
+        buyer.getAccount()
+             .setPassword(passwordEncoder.encode(username));
+        return User.builder()
+                   .username(buyer.getUsername())
+                   .password(buyer.getPassword())
+                   .roles("BUYER")
+                   .build();
+    }
+    /*
+        @Transactional
+    public AdminDto updateProfile(Long id, AdminDto adminDto) {
+        Admin currentAdmin = adminRepository.findById(id)
+                                            .orElseThrow(() -> new RecordNotFoundException("Admin", "Id", id
+                                                    .toString()));
+        Address address = Address.builder()
+                                 .country(adminDto.country())
+                                 .zipcode(adminDto.zipCode())
+                                 .street(adminDto.street())
+                                 .city(adminDto.city())
+                                 .build();
+        String encodedPassword = passwordEncoder.encode(adminDto.password());
+        Account account = Account.builder()
+                                 .name(adminDto.name())
+                                 .address(address)
+                                 .password(encodedPassword)
+                                 .birthday(adminDto.dob())
+                                 .job(adminDto.job())
+                                 .phoneNumber(adminDto.phoneNumber())
+                                 .email(adminDto.email())
+                                 .build();
+        currentAdmin.setAccount(account);
+        currentAdmin = adminRepository.save(currentAdmin);
+        return adminMapper.toDto(currentAdmin);
+    }
+     */
 }
