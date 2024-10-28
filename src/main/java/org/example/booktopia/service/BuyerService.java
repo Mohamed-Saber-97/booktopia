7af2844527ca29119c7c5127ff933e7c22027c78
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-28 16:55:40 +0300

    fix security bug

--- src/main/java/org/example/booktopia/service/BuyerService.java
+++ src/main/java/org/example/booktopia/service/BuyerService.java
@@ -1,37 +1,37 @@
 package org.example.booktopia.service;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.booktopia.controller.PaymobController;
 import org.example.booktopia.dtos.BuyerDto;
 import org.example.booktopia.dtos.OrderDto;
-import org.example.booktopia.error.InsufficientFundsException;
-import org.example.booktopia.error.InsufficientStockException;
 import org.example.booktopia.error.RecordNotFoundException;
 import org.example.booktopia.mapper.BuyerMapper;
 import org.example.booktopia.mapper.CategoryMapper;
 import org.example.booktopia.mapper.OrderMapper;
 import org.example.booktopia.model.Buyer;
-import org.example.booktopia.model.Order;
-import org.example.booktopia.model.OrderProduct;
 import org.example.booktopia.repository.BuyerRepository;
 import org.example.booktopia.utils.RequestBuilderUtil;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Map;
 
 import static org.example.booktopia.utils.RequestAttributeUtil.USER;
 
@@ -49,13 +49,23 @@ public class BuyerService implements UserDetailsService {
 
     public Buyer findById(Long id) {
         return buyerRepository.findById(id)
-                .orElseThrow(() -> new RecordNotFoundException("Buyer", "ID", id.toString()));
+                              .orElseThrow(() -> new RecordNotFoundException("Buyer", "ID", id.toString()));
     }
 
     @Transactional
     public BuyerDto save(HttpServletRequest request) {
         Buyer buyer = requestBuilderUtil.createBuyerFromRequest(request);
         buyer = buyerRepository.save(buyer);
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_BUYER"));
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                buyer.getUsername(),
+                buyer.getPassword(),
+                authorities
+        );
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+        HttpSession session = request.getSession(true);
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, securityContext);
         return buyerMapper.toDto(buyer);
     }
 
@@ -74,15 +84,15 @@ public class BuyerService implements UserDetailsService {
 
     public BuyerDto findByEmail(String email) {
         Buyer buyer = buyerRepository.findByAccountEmail(email)
-                .orElseThrow(() -> new RecordNotFoundException("Buyer", "Email", email));
+                                     .orElseThrow(() -> new RecordNotFoundException("Buyer", "Email", email));
         return buyerMapper.toDto(buyer);
     }
 
     public BuyerDto findByPhoneNumber(String phoneNumber) {
         Buyer buyer = buyerRepository.findByAccountPhoneNumber(phoneNumber)
-                .orElseThrow(() -> new RecordNotFoundException("Buyer",
-                        "Phonenumber",
-                        phoneNumber));
+                                     .orElseThrow(() -> new RecordNotFoundException("Buyer",
+                                                                                    "Phonenumber",
+                                                                                    phoneNumber));
         return buyerMapper.toDto(buyer);
     }
 
@@ -90,20 +100,20 @@ public class BuyerService implements UserDetailsService {
 //    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Buyer buyer = buyerRepository.findByAccountEmailAndIsDeletedIsFalse(username)
-                .orElseThrow(() -> new UsernameNotFoundException(username));
+                                     .orElseThrow(() -> new UsernameNotFoundException(username));
 //        buyer.getAccount()
 //             .setPassword(passwordEncoder.encode(username));
         return User.builder()
-                .username(buyer.getUsername())
-                .password(buyer.getPassword())
-                .roles("BUYER")
-                .build();
+                   .username(buyer.getUsername())
+                   .password(buyer.getPassword())
+                   .roles("BUYER")
+                   .build();
     }
 
     @Transactional
     public BuyerDto updateProfile(HttpServletRequest request) {
         Long id = ((BuyerDto) request.getSession()
-                .getAttribute(USER)).id();
+                                     .getAttribute(USER)).id();
         Buyer currentBuyer = findById(id);
         Buyer requestBuyer = requestBuilderUtil.updateBuyerFromRequest(currentBuyer, request);
         buyerRepository.save(requestBuyer);
@@ -113,15 +123,16 @@ public class BuyerService implements UserDetailsService {
     public List<OrderDto> getOrdersByBuyerId(Long id) {
         Buyer buyer = this.findById(id);
         return buyer.getOrders()
-                .stream()
-                .map(orderMapper::toDto).toList();
+                    .stream()
+                    .map(orderMapper::toDto)
+                    .toList();
     }
 
     public List<BuyerDto> getAllBuyers(Integer pageNumber, Integer pageSize) {
         return buyerRepository.findAll(PageRequest.of(pageNumber, pageSize))
-                .stream()
-                .map(buyerMapper::toDto)
-                .toList();
+                              .stream()
+                              .map(buyerMapper::toDto)
+                              .toList();
     }
 
 //    @Transactional
